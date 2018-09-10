4a61db94e9a73f01de836468519ff11a2f8543ef
David Hockley <david.hockley@gmail.com>
2018-09-10 07:21:46 +0200

    add Dot, update debug

--- lua/plugin/shapes.lua
+++ lua/plugin/shapes.lua
@@ -3,40 +3,58 @@ local Library = require "CoronaLibrary"
 -- Create library
 local lib = Library:new{ name='plugin.shapes', publisherId='com.kodaps' }
 
+display.setDefault( "isAnchorClamped", false )
+
 -------------------------------------------------------------------------------
 -- BEGIN (Insert your implementation starting here)
 -------------------------------------------------------------------------------
 
 local function deg2rad(angle)
 	angle = angle or 0
-    return math.pi * angle/180
+    return math.pi * (angle/180 - .5)
 end
 
 local cos = math.cos
 local sin = math.sin
 
+local function dcos(dangle)
+    return cos(deg2rad(dangle))
+end
+
+local function dsin(dangle)
+    return sin(deg2rad(dangle))
+end
+
+
 local function arc(start_angle, end_angle, radius, step)
 
-    local _start = deg2rad(start_angle)
-    local _end = deg2rad(end_angle)
 
-    local bit = deg2rad(step or 5)
-
-    if _start > _end then
-        local _ = _end
-        _end = _start
-        _start = _
+    if start_angle > end_angle then 
+        local _ = end_angle
+        end_angle = start_angle
+        start_angle = _
     end
 
-    local points = {}
-    local _count = 1
-    local _curr = _start
+    step = step or 5
 
-    while (_curr <= _end) do
-        points[_count] = radius*cos(_curr)
-        points[_count+1] = radius*sin(_curr)
-        _count = _count +2
-        _curr = _curr + bit
+    --local _start = deg2rad(start_angle)
+    --local _end = deg2rad(end_angle)
+
+    --local bit = deg2rad(step or 5)
+
+    --if _start > _end then
+    --   local _ = _end
+    --    _end = _start
+    --    _start = _
+    --end
+    local points = {}
+
+    local _count = 1
+
+    for deg = start_angle, end_angle, step do
+        points[_count] = radius*dcos(deg)
+        points[_count+1] = radius*dsin(deg)
+        _count = _count + 2
     end
 
     return points
@@ -45,6 +63,20 @@ local function arc(start_angle, end_angle, radius, step)
 end
 
 
+function lib.newDot(data)
+
+	data = data or {}
+
+    local mat = display.newCircle(data.x or 0, data.y or 0, 3)
+    mat:setFillColor(1,0,0)
+
+    if data.parent then
+        data.parent:insert(mat)
+    end
+
+	return mat
+
+end
 
 function lib.newArc (data)
 
@@ -54,6 +86,8 @@ function lib.newArc (data)
 
     local mat = display.newLine(unpack(ret))
     mat.strokeWidth = data.stroke or 1
+
+    print("arc anchors", mat.anchorX, mat.anchorY)
 
     if data.parent then
         data.parent:insert(mat)
@@ -69,11 +103,52 @@ function lib.newPie (data)
 
 	local ret = arc(data.start_angle, data.end_angle, data.radius)
 
+    local angle = math.abs(data.end_angle - data.start_angle)
+
+    -- if the angle acute, etc. ?
+    -- and which quadran is the angle in ?
+
+    -- if angle = 180 then it is in the center 
+
+    -- 
+
+    local x_start = dcos(data.start_angle)
+    local x_end = dcos(data.end_angle)
+
+    local y_start = dsin(data.start_angle)
+    local y_end = dsin(data.end_angle)
+
+    local x = x_start - x_end
+    -- local xmid = 
+
+
+    local x_length = x_start - x_end
+
+    local anchorX, anchorY = 0.5,0.5
+
+
+    if angle < 180 then
+
+
+        anchorY = 0
+
+        if angle < 90 then
+            anchorX = 0
+        end
+    end
+
+
 	table.insert(ret, 1, 0)
-	table.insert(ret, 1, 0)
+    table.insert(ret, 1, 0)
+
 
     local mat = display.newPolygon(0,0, ret)
     mat.strokeWidth = data.stroke or 1
+    mat.anchorX, mat.anchorY = anchorX, anchorY
+
+    _x, _y = mat:contentToLocal(0, 0)
+    print(_x, _y)
+
 
     if data.parent then
         data.parent:insert(mat)
